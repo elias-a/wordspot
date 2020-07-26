b762640338f1f3f83447f1ed7fe547b001edfeef
elias-a <elias.alstead@gmail.com>
2020-07-25 22:32:46 -0400

    Add player model

--- api/models.ts
+++ api/models.ts
@@ -16,8 +16,9 @@ const tiles = [
 
 export class Models {
     sequelize: Sequelize;
-    Game: any;
+    Player: any;
     Tile: any;
+    Game: any;
 
     constructor(sequelize: Sequelize) {
         this.sequelize = sequelize;
@@ -26,8 +27,84 @@ export class Models {
             console.log("Failed to connect to database");
         }
 
+        this.initPlayer();
         this.initGame();
         this.initTile();
+    }
+
+    async initPlayer() {
+        this.Player = this.sequelize.define('Player', {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            name: {
+                type: DataTypes.STRING
+            },
+            turn: {
+                type: DataTypes.INTEGER
+            },
+            tokens: {
+                type: DataTypes.INTEGER
+            }
+        }, {
+            tableName: 'Player',
+            timestamps: false 
+        });
+
+        await this.Player.sync();
+
+        if (!(await this.Player.findAll()).length) {
+            await this.Player.create({
+                name: "Player 1",
+                turn: 1,
+                tokens: 26
+            });
+            await this.Player.create({
+                name: "Player 2",
+                turn: 0,
+                tokens: 25
+            })
+        }
+    }
+
+    async initTile() {
+        this.Tile = this.sequelize.define('Tile', {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            letters: {
+                type: DataTypes.STRING
+            },
+            location: {
+                type: DataTypes.INTEGER
+            },
+            game: {
+                type: DataTypes.INTEGER,
+                references: {
+                    model: 'Game',
+                    key: 'id'
+                }
+            }
+        }, {
+            tableName: "Tile",
+            timestamps: false
+        });
+
+        await this.Tile.sync();
+
+        if (!(await this.Tile.findAll()).length) {
+            const perms = [...Array(16).keys()].sort(() => Math.random() - 0.5);
+            await Promise.all(perms.map(async (num, idx) => {
+                await this.Tile.create({
+                    letters: tiles[Math.floor(Math.random() * tiles.length)],
+                    location: idx
+                });
+            }));
+        }
     }
 
     async initGame() {
@@ -51,37 +128,6 @@ export class Models {
             await this.Game.create({
                 name: "game1"
             });
-        }
-    }
-
-    async initTile() {
-        this.Tile = this.sequelize.define('Tile', {
-            id: {
-                type: DataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true
-            },
-            letters: {
-                type: DataTypes.STRING
-            },
-            location: {
-                type: DataTypes.INTEGER
-            }
-        }, {
-            tableName: "Tile",
-            timestamps: false
-        });
-
-        await this.Tile.sync();
-
-        if (!(await this.Tile.findAll()).length) {
-            const perms = [...Array(16).keys()].sort(() => Math.random() - 0.5);
-            await Promise.all(perms.map(async (num, idx) => {
-                await this.Tile.create({
-                    letters: tiles[Math.floor(Math.random() * tiles.length)],
-                    location: idx
-                });
-            }));
         }
     }
 }
