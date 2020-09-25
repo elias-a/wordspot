360af62985c15e83b4aaa6378c48c001ea265c59
Elias Alstead <elias-a@eliass-air.myfiosgateway.com>
2020-09-25 18:37:50 -0400

    Add form elements to login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Login from './auth/Login';
 import Game from './game/Game';
 
 function App() {
     return (
       <div>
         <h1>Wordspot</h1>
-        <Game />
+        <Login />
       </div>  
     );
 }
--- src/auth/login.tsx
+++ src/auth/login.tsx
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import {
+    TextField,
+    Button
+} from '@material-ui/core';
 import { useStyles } from '../styles';
 
+const INITIAL_USER = {
+    username: '',
+    password: ''
+};
+
 function Login() {
+    const [user, setUser] = useState(INITIAL_USER);
+    const [disabled, setDisabled] = useState(true);
     const styles = useStyles();
 
+    useEffect(() => {
+        Object.values(user).every(param => param !== '') ?
+            setDisabled(false) : setDisabled(true);
+    }, [user]);
+
+    const handleChange = (event: any) => {
+        const { id, value } = event.target;
+        setUser(prevState => ({ ...prevState, [id]: value }));
+    }
+
+    const handleSubmit = () => {
+
+    }
+
     return (
-        <React.Fragment></React.Fragment>
+        <div>
+            <TextField 
+                id="username" 
+                label="Username" 
+                value={user.username}
+                variant="outlined" 
+                required
+                onChange={handleChange}
+            />
+            <TextField 
+                id="password" 
+                type="password"
+                label="Password" 
+                value={user.password}
+                variant="outlined" 
+                required
+                onChange={handleChange}
+            />
+            <Button
+                className={styles.button}
+                disabled={disabled}
+                onClick={handleSubmit}
+            >
+                {"Login"}
+            </Button>
+        </div>
     );
 }
+
+export default Login;
